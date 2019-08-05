c78532decb0a1db2d34518bb0f1e14c621334099
meryusha <merey.ramazanova@nu.edu.kz>
2019-08-05 13:55:44 +0300

    rm

--- compile.sh
+++
@@ -1,6 +0,0 @@
-echo "Compile Qt GUI files"
-pyuic5 -x src/gui/mainwindow.ui -o src/gui/ui_mainwindow.py
-pyrcc5 src/resources.qrc -o src/resources.py
-
-echo "Run source"
-python src/ActiveImageLabeler.py
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-echo "Activate Env"
-source activate ActiveImageLabeler
- 
-echo "Run source"
-python src/ActiveImageLabeler.py
